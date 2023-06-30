317e1110cba9f58e025be3e6435c03dd25f197ca
shivamjha <shivamjha2101@gmail.com>
2023-06-30 20:40:13 +0530

    hello

--- QUEUE/PRIORITYQUEUE/LinkedList/1.cpp
+++ QUEUE/PRIORITYQUEUE/LinkedList/1.cpp
@@ -1,69 +1,72 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
+#include <conio.h>
+
 struct node
 {
-    int data;
-    int priority;
-    struct node *next;
+	int data;
+	int priority;
+	struct node *next;
 };
-void addNode(struct node **head, int data, int priority)
+
+void addData(struct node **head, int data, int priority)
 {
-    struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->data = data;
-    temp->priority = priority;
-    if ((*head)==NULL)
-    {
-        temp->next = NULL;
-        (*head) = temp;
-    }
-    else{
-    	struct node *temp_1 = (*head);
- 		if(temp->priority>temp_1->priority)
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->data = data;
+	temp->priority = priority;
+	if ((*head) == NULL)
+	{
+		temp->next = NULL;
+		*head = temp;
+	}
+	else
+	{
+		struct node *temp_1 = (*head);
+		if(temp_1->priority<temp->priority)
 		{
 			temp->next = temp_1;
-			*head= temp;
+			*head =  temp;
 		}
 		else{
 			while(temp_1->next!=NULL)
 			{
-				if(temp_1->next->priority < temp->priority)
+				if(temp_1->next->priority<temp->priority)
 				{
-					temp->next = temp_1->next;
-					temp_1->next=temp;
-				}
-				else{
-					temp->next= NULL;
-					temp_1->next = temp;
+					temp_1 = temp_1->next;
 				}
 			}
-		}   	
-	}
-}
-
-void displayData(struct node *head)
-{
-    if(head==NULL)
-    {
-    	printf("\nUnderflow");
-	}
-	else{
-		struct node *temp=head;
-		while(temp!=NULL)
-		{
-			printf("\n%d",temp->data);
-			printf("\n%d",temp->priority);
-			temp = temp->next;
+			temp->next = temp_1->next;
+			temp_1 = temp;
 		}
 	}
 }
-
+void displayData(struct node *head)
+{
+	if (head == NULL)
+	{
+		printf("\nUnderflow");
+	}
+	else
+	{
+		while (head != NULL)
+		{
+			printf("\n%d", head->data);
+			head = head->next;
+		}
+	}
+}
 int main()
 {
-    struct node *head = NULL;
-    addNode(&head, 11, 1);
-    addNode(&head, 12, 3);
-    addNode(&head, 1, 1);
-    displayData(head);
-    return 0;
+	struct node *head = NULL;
+	addData(&head, 11, 4);
+	addData(&head, 12, 5);
+	addData(&head, 13, 6);
+	addData(&head, 14, 7);
+	addData(&head, 15, 8);
+	addData(&head, 15, 3);
+	addData(&head, 15, 2);
+	addData(&head, 15, 1);
+	
+	displayData(head);
+	return 0;
 }
